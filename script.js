076d8ead21dec22501ca37a019233267c2a84164
dezoliveira <andresoliveira@protonmail.com>
2021-09-26 11:45:17 -0300

    randommeal (busca rango) v1.0

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ getMealBtn.addEventListener('click', () => {
 })
 
 function createMeal(meal){
-  const ingredients = [] //Looping para pegar os ingredientes e tacar num array
-  for(i=1; i<=20; i++){
+  const ingredients = [] //Looping para pegar os ingredientes do objeto e tacar num array
+  for(let i=1; i<=20; i++){
     if(meal[`strIngredient${i}`]){
       ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`) //Ingrediente + Quantidade
     } else{
-        break
+        break //Para se não encontrar mais ingredientes
     }
   }
 
@@ -24,9 +24,9 @@ function createMeal(meal){
   <div class="meal-food">
     <div class="meal-info">
       <img src="${meal.strMealThumb}" alt="Meal Img" />
-      <p><strong> Categoria: </strong>${meal.strCategory}</p>
-      <p><strong> Area: </strong>${meal.strArea}</p>
-      <p><strong> Tags: </strong>${meal.strTags.split(',').join(', ')}</p>
+      ${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+			${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+			${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
 
       <h5>Ingredientes</h5>
       <ul>
